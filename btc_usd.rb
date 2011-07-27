3e1cab360925cee82321362c7a582dadddd046b1
Giles Bowkett <gilesb@gmail.com>
2011-07-26 22:48:50 -0700

    error pagificationating

--- btc_usd.rb
+++ btc_usd.rb
@@ -1,11 +1,20 @@
-require 'rubygems'
-require 'rest_client'
-require 'json'
-require 'erb'
+%w{rubygems rest_client json erb}.each {|lib| require lib}
+
+class File
+  class << self
+    def here(path)
+      expand_path(dirname(__FILE__)) + path
+    end
+  end
+end
 
 @price = JSON.parse(RestClient.get("http://bitcoincharts.com/t/weighted_prices.json"))["USD"]["24h"]
 
-File.open(File.expand_path(File.dirname(__FILE__)) + "/public/index.html", "w") do |file|
-  file.puts ERB.new(File.read(File.expand_path(File.dirname(__FILE__)) + "/template.erb")).result(binding)
+File.open(File.here("/public/index.html"), "w") do |file|
+  file.puts ERB.new(File.read(File.here("/template.erb"))).result(binding)
+end
+
+if File.new(File.here("/public/index.html")).stat.zero?
+  system("cp error_page.html public/index.html")
 end
 
